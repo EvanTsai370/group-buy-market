bc46164f380bdf385f49b82d810ac5ec2ac6d23d
Evan <graysoncyb@gmail.com>
2026-01-12 20:28:11 +0800

    refactor: 移动SecurityConfig

--- my-group-by-market-infrastructure/src/main/java/org/example/infrastructure/security/SecurityConfig.java
+++ my-group-by-market-infrastructure/src/main/java/org/example/infrastructure/security/SecurityConfig.java
@@ -1,7 +1,6 @@
-package org.example.config;
+package org.example.infrastructure.security;
 
 import lombok.RequiredArgsConstructor;
-import org.example.infrastructure.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
